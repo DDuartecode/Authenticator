4e0c864d08b80af9c070fb857a67feb5ef696840
Daniel Duarte <duartesx@hotmail.com>
2025-07-09 03:17:01 -0300

    ajuste expiração token

--- AuthenticatorApi/app/Providers/AppServiceProvider.php
+++ AuthenticatorApi/app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
 use Laravel\Passport\Passport;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::personalAccessTokensExpireIn(now()->addDay());
     }
 }
